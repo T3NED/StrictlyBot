9d42501b9ac0e56fe402cf7330c531efd30f76eb
T3NED <software@t3ned.com>
2021-10-22 20:59:15 +0100

    ✨ Link providers to #lib

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,5 +4,11 @@ export * as config from "../config";
 // Errors
 export * from "./errors/MissingEnvVariableError";
 
+// Providers
+export * from "./providers/helpers/DatabaseHelper";
+export * from "./providers/helpers/RedisHelper";
+export * from "./providers/DatabaseProvider";
+export * from "./providers/RedisProvider";
+
 // Utils
 export * from "./utils/Logger";
